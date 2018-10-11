a0081244abf88f47d1a316294264989d81c17e38
Karl Arnar Ægisson <karla16@ru.is>
2018-10-11 20:00:21 +0000

    Refactor of code

--- StringCalculator.js
+++ StringCalculator.js
@@ -5,16 +5,20 @@ function Add(numbers) {
 
 	else if(numbers.includes(",")) {
 		var arr = numbers.split(",");
-		var sum = 0;
-		for(var i = 0; i < arr.length; i++) {
-			sum += parseInt(arr[i]);
-		}
-		return sum;
+		return Sum(arr);
 	}
 
 	else {
 		return parseInt(numbers);
 	}
 };
+
+function Sum(numbers) {
+	var sum = 0;
+	for(var i = 0; i < numbers.length; i++) {
+		sum += parseInt(numbers[i]);
+	}
+	return sum;
+}
 
 module.exports = Add;
