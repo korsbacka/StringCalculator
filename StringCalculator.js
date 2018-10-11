99cf4f5058145f342a3565fdd8a5060bfc4423bf
Karl Arnar Ægisson <karla16@ru.is>
2018-10-11 19:44:50 +0000

    Single number returns number, tested and passed

--- StringCalculator.js
+++ StringCalculator.js
@@ -2,6 +2,10 @@ function Add(numbers) {
 	if(numbers == "") {
 		return 0;
 	}
+
+	else {
+		return parseInt(numbers);
+	}
 };
 
 module.exports = Add;
--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -3,3 +3,11 @@ const Add = require('./StringCalculator');
 test("An empty string should return 0", () => {
 	expect(Add("")).toBe(0);
 });
+
+test("A single number returns the number", () => {
+	expect(Add("1")).toBe(1);
+});
+
+test("Empty string and number returns the number", () => {
+	expect(Add(" 2")).toBe(2);
+});
